bf56e9079107a5ec1dabc111314b6e6c7316e4b1
mustafa-onay <onay68@gmx.at>
2024-09-04 12:51:32 +0200

    changes in Objects

--- day02/07Objects.js
+++ day02/07Objects.js
@@ -30,5 +30,39 @@ let laptop = {
         "Intel Core i7 processor"
     ]
 }
-
 console.log(laptop.specs[0]); // Output: "16GB RAM"
+
+let calculator = {
+    add: function (num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function (num1, num2) {
+        return num1 - num2;
+    }
+}
+
+console.log(calculator.add(5, 3)); // Output: 8
+
+// complex object
+
+let university = {
+    name: "Harward",
+    location: "Cambridge, Massachusetts, USA",
+    established: 1636,
+    departments: ["Computer Science", "Mathematics", "Physics"],
+    ranking: {
+        world: 1,
+        national: 1
+    },
+    getInfo: function(){
+        return "The University of " + this.name + " is located in " + this.location + "and was established in " + this.established + ".";
+    }
+}
+
+console.log(university.getInfo()); // Output: "The University of Harvard is located in Cambridge, Massachusetts, USA
+
+delete laptop.model
+
+console.log("model" in laptop); // Output: false
+console.log("brand" in laptop); // Output: true
+
